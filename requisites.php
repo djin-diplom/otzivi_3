94161e1315f4ce845c6b79ddcc9e89161b72502d
21vek <git@github.com>
2017-08-27 20:41:13 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,12 +3,12 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = "http://vivadip.xyz";//'http://studrabota.com';//"/projects/landingpages/otzivi";
-$donor_name = 'http://studotzyv.ru';
+$main_name = "http://uznai.esy.es";//'http://studrabota.com';//"/projects/landingpages/otzivi";
+$donor_name = 'http://uznaika.su';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
 $donor_site_name_3 = 'Cтуд-Отзыв';
-$donor_site_name_4 = 'add@studotzyv.ru';
+$donor_site_name_4 = 'add@uznaika.su';
 //$main_name_corot = "BYPolit.org";
-$site_name = "Вива Диплом";
-$site_name_2 = "add@viva-diplom.ru";
+$site_name = "Узнай";
+$site_name_2 = "add@uznai.esy.es";
