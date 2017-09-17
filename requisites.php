e8d06d45c19532d5dfa45e0849f4e4b46a006808
21vek <git@github.com>
2017-09-17 11:58:25 +0300

    1

--- requisites.php
+++ requisites.php
@@ -12,3 +12,17 @@ $donor_site_name_4 = 'add@uznaika.su';
 //$main_name_corot = "BYPolit.org";
 $site_name = "Узнай";
 $site_name_2 = "add@uznai.esy.es";
+
+
+$raskrutka_site_1 = 'znaniumbaza.ru';
+$raskrutka_site_2 = 'znaniumbaza';
+$raskrutka_site_3 = 'ZNANIUMBAZA.RU';
+$raskrutka_site_4 = 'znaniumbaza_ru';
+$raskrutka_site_5 = 'ZNANIUMBAZA';
+$raskrutka_site_6 = 'Znaniumbaza';
+$raskrutka_site_7 = 'Znaniumbaza.ru';
+
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Лермонтова, дом № 8, oф. 184-185';
+
+$yandex_google = '<meta name="google-site-verification" content="Iaw89eWBIIKiEFhk2jQcF7oKFdT-b7qLIMak1dQsGcM" />
+<meta name="yandex-verification" content="3ddb091b023e7c6b" />';
--- router.php
+++ router.php
@@ -1,9 +1,9 @@
 <?php
 
 function rand_podstav_smv($text){
-      //$text = str_replace('х','x',$text);
-     //$text = str_replace('а','a',$text);
-    //$text = str_replace('р','p',$text);
+      $text = str_replace('х','x',$text);
+     $text = str_replace('а','a',$text);
+    $text = str_replace('р','p',$text);
    //$text = str_replace('о','o',$text);
    //$text = str_replace('е','e',$text);
     //$text = str_replace('с','c',$text);
@@ -23,7 +23,7 @@ $REQUEST_URI = str_replace($main_name, $donor_name, $REQUEST_URI );
 
 $REQUEST_URI = str_replace( 'rosdiplom', '', $REQUEST_URI  );//new
 
-$contentTitle = str_replace('znaniumbaza', 'rosdiplom', $REQUEST_URI );
+$contentTitle = str_replace($raskrutka_site_2, 'rosdiplom', $REQUEST_URI );
 
 //echo $contentTitle;
 
@@ -34,100 +34,79 @@ $mainContent = file_get_contents($contentTitle);
 
 $mainContent = str_replace('http://uznaika.su/wp-content/uploads/2015/09/Rosdiplom.jpg', $main_name.'/fi/baza.png', $mainContent  );
 
-//$mainContent = str_replace('url(/fi', 'url('.$donor_name.'/fi', $mainContent  );
-//$mainContent = str_replace('src="http', 'gfhtyur', $mainContent  );
-//$mainContent = str_replace('href="http', 'gftyur_2', $mainContent  );
-//$mainContent = str_replace('mobile-title" href="http://studotzyv.ru/"', 'qoferiid', $mainContent  );
 $mainContent = str_replace("src='http", 'sfdfhla', $mainContent  );
 $mainContent = str_replace("href='http", 'miiahfd', $mainContent  );
 $mainContent = str_replace("http://uznaika.su/wp-content/", 'qheijdifj', $mainContent  );
 $mainContent = str_replace($donor_name, $main_name, $mainContent  );
-//$mainContent = str_replace('gfhtyur', 'src="http',  $mainContent  );
-//$mainContent = str_replace('gftyur_2','href="http',  $mainContent  );
+
 $mainContent = str_replace('sfdfhla',"src='http",  $mainContent  );
 $mainContent = str_replace('miiahfd', "href='http", $mainContent  );
 $mainContent = str_replace('qheijdifj', "http://uznaika.su/wp-content/", $mainContent  );
-//$mainContent = str_replace('src="', 'src="'.$donor_name, $mainContent  );
-//$mainContent = str_replace('href="/templates/', 'href="'.$donor_name.'/templates/', $mainContent  );
-//$mainContent = str_replace('href="/3d-corner-ribbons.css"', 'href="'.$donor_name.'/3d-corner-ribbons.css"', $mainContent  );
-//$mainContent = str_replace('href="/favicon.png"', 'href="'.$donor_name.'/favicon.png"', $mainContent  );
-//$mainContent = str_replace('gfhtyur', 'src="http', $mainContent  );
-//$mainContent = str_replace('http://xn--d1acynfdde.xn--p1ai/engine/modules/antibot/antibot.php', $main_name.'/fi/capcha.jpg', $mainContent  );
+
 $mainContent = str_replace($donor_site_name, $site_name, $mainContent  );
 $mainContent = str_replace($donor_site_name_2, $site_name, $mainContent  );
 $mainContent = str_replace($donor_site_name_3, $site_name, $mainContent  );
 $mainContent = str_replace($donor_site_name_4, $site_name_2, $mainContent  );
 
-$mainContent = str_replace('Росдиплома', 'Базы знаний', $mainContent  );
-$mainContent = str_replace('Росдиплому', 'Базе знаний', $mainContent  );
-$mainContent = str_replace('Росдипломе', 'Базе знаний', $mainContent  );
-$mainContent = str_replace('Росдипломом', 'Базой знаний', $mainContent  );
-$mainContent = str_replace('Росдиплом', 'База знаний', $mainContent  );
-$mainContent = str_replace('РОСДИПЛОМ', 'БАЗА ЗНАНИЙ', $mainContent  );
-$mainContent = str_replace('Рос Диплому', 'Базе Знаний', $mainContent  );
-$mainContent = str_replace('Рос Диплом', 'База знаний', $mainContent  );
-$mainContent = str_replace('росдипломовцы', 'базазнановцы', $mainContent  );
-$mainContent = str_replace('росдиплома', 'базы знаний', $mainContent  );
-$mainContent = str_replace('росдиплому', 'базе знаний', $mainContent  );
-$mainContent = str_replace('росдипломе', 'базе знаний', $mainContent  );
-$mainContent = str_replace('росдипломом', 'базой знаний', $mainContent  );
-$mainContent = str_replace('росдиплом', 'база знаний', $mainContent  );
-$mainContent = str_replace('РосДиплома', 'Базы Знаний', $mainContent  );
-$mainContent = str_replace('РосДиплому', 'Базе Знаний', $mainContent  );
-$mainContent = str_replace('РосДипломе', 'Базе Знаний', $mainContent  );
-$mainContent = str_replace('РосДипломом', 'Базой Знаний', $mainContent  );
-$mainContent = str_replace('Роспдипломе', 'Базе Знаний', $mainContent  );
-$mainContent = str_replace('РосДиплом', 'База Знаний', $mainContent  );
-//$mainContent = str_replace('www.rosdiplom.ru', 'gsgdfdfd', $mainContent  );
-$mainContent = str_replace('rosdiplom.ru', 'znaniumbaza.ru', $mainContent  );
+$mainContent = str_replace('Росдиплома', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('Росдиплому', $raskrutka_site_2, $mainContent  );
+$mainContent = str_replace('Росдипломе', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('Росдипломом', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('Росдиплом', $raskrutka_site_2, $mainContent  );
+$mainContent = str_replace('РОСДИПЛОМ', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('Рос Диплому', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('Рос Диплом', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('росдипломовцы', 'работники '.$raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('росдиплома', $raskrutka_site_2, $mainContent  );
+$mainContent = str_replace('росдиплому', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('росдипломе', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('росдипломом', $raskrutka_site_2, $mainContent  );
+$mainContent = str_replace('росдиплом', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('РосДиплома', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('РосДиплому', $raskrutka_site_2, $mainContent  );
+$mainContent = str_replace('РосДипломе', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('РосДипломом', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('Роспдипломе', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('РосДиплом', $raskrutka_site_1, $mainContent  );
+$mainContent = str_replace('rosdiplom.ru', $raskrutka_site_1, $mainContent  );
 
 
-$mainContent = str_replace('Rosdiplom.ru', 'Znaniumbaza.ru', $mainContent  );
-$mainContent = str_replace('ROSDIPLOM.RU', 'ZNANIUMBAZA.RU', $mainContent  );
-$mainContent = str_replace('RosDiplom', 'ZnaniumBaza', $mainContent  );
-$mainContent = str_replace('ROSDIPLOM', 'ZNANIUMBAZA', $mainContent  );
+$mainContent = str_replace('Rosdiplom.ru', $raskrutka_site_7, $mainContent  );
+$mainContent = str_replace('ROSDIPLOM.RU', $raskrutka_site_3, $mainContent  );
+$mainContent = str_replace('RosDiplom', $raskrutka_site_6 , $mainContent  );
+$mainContent = str_replace('ROSDIPLOM', $raskrutka_site_5 , $mainContent  );
 
 
-$mainContent = str_replace('Rosdiplom', 'Znaniumbaza', $mainContent  );
-$mainContent = str_replace('rosdiplom', 'znaniumbaza', $mainContent  );
+$mainContent = str_replace('Rosdiplom', $raskrutka_site_6, $mainContent  );
+$mainContent = str_replace('rosdiplom', $raskrutka_site_2, $mainContent  );
 $mainContent = str_replace("src='/wp-content/", "src='".$donor_name."/wp-content/", $mainContent  );
-$mainContent = str_replace('Москва, ул. Сретенка, д.27/29, стр 1', 'г. Санкт-Петербург, ул. Лермонтова, дом № 8, oф. 184-185', $mainContent  );
+$mainContent = str_replace('Москва, ул. Сретенка, д.27/29, стр 1', $raskrutka_site_adress, $mainContent  );
 $mainContent = str_replace('+7 (495) 987-41-36', 'Компания с 2013 года работает без телефонных менеджеров', $mainContent  );
 $mainContent = str_replace('74959874136', '', $mainContent  );
 $mainContent = str_replace('<span class="city">Волгоград:</span> +78442 986161', 'Используйте форму заказа на сайте, пишите на почту', $mainContent  );
-$mainContent = str_replace('http://studotzyv.ru/rel.php?id=aHR0cDovL3d3dy5yb3NkaXBsb20ucnU=', 'http://znaniumbaza.ru/', $mainContent  );
-$mainContent = str_replace('http://studotzyv.ru/rel.php?id=aHR0cDovL3Jvc2RpcGxvbS5ydS96YWthei9mYXN0Lz9yZWY9c3R1ZG90enl2', 'http://znaniumbaza.ru/order.php', $mainContent  );
+$mainContent = str_replace('http://studotzyv.ru/rel.php?id=aHR0cDovL3d3dy5yb3NkaXBsb20ucnU=', 'http://'.$raskrutka_site_1.'/', $mainContent  );
+$mainContent = str_replace('http://studotzyv.ru/rel.php?id=aHR0cDovL3Jvc2RpcGxvbS5ydS96YWthei9mYXN0Lz9yZWY9c3R1ZG90enl2', 'http://'.$raskrutka_site_1.'/order.php', $mainContent  );
 
-//$mainContent = str_replace('znaniumbaza', 'rosdiplom', $mainContent  );
+
 $mainContent = str_replace('Публикации</a>', '</a>', $mainContent  );
 $mainContent = str_replace('это интересно</a>', '</a>', $mainContent  );
 
-$mainContent = str_replace('</title>', '</title> 
-<meta name="google-site-verification" content="Iaw89eWBIIKiEFhk2jQcF7oKFdT-b7qLIMak1dQsGcM" />
-<meta name="yandex-verification" content="3ddb091b023e7c6b" />', $mainContent  );
-//$mainContent = str_replace('gsgdfdfd', 'www.rosdiplom.ru',  $mainContent  );
-//$mainContent = str_replace('qoferiid','mobile-title" href="http://studotzyv.ru/"', $mainContent  );
+$mainContent = str_replace('</title>', '</title> '.$yandex_google, $mainContent  );
+
 $mainContent = str_replace('src="/wp-content/','src="'.$donor_name.'/wp-content/', $mainContent  );
 $mainContent = str_replace('Представительства и офисы компании','Представительства и офисы компании ', $mainContent  );
-$mainContent = str_replace('Москва, ул.Сретенка, д.27/29, стр 1','Санкт-Петербург, ул.Лермонтова, д.8', $mainContent  );
 $mainContent = str_replace('gglcptch_recaptcha','', $mainContent  );
 $mainContent = str_replace('wp-comments-post.php','', $mainContent  );
 $mainContent = str_replace('VK.Widgets.Group','', $mainContent  );
 $mainContent = str_replace('please-add','', $mainContent  );
 $mainContent = str_replace("href='http://studotzyv.ru/raiting","href='".$main_name."/raiting", $mainContent  );
 $mainContent = str_replace("href='http://studotzyv.ru/otzyv","href='".$main_name."/otzyv", $mainContent  );
-$mainContent = str_replace("55.753994, 37.622093", "59.76351, 30.35746", $mainContent  );
 $mainContent = str_replace('icon-home">', 'icon-home">Главная', $mainContent  );
 $mainContent = str_replace('карта-сайта', 'wlfjekgjt', $mainContent );
 $mainContent = str_replace('src="/images/', 'src="'.$donor_name.'/images/', $mainContent );
 $mainContent = str_replace('class="job_filters"', 'action="'.$main_name.'"', $mainContent );
 $mainContent = str_replace("class='comment-reply-link' href='http://uznaika.su", "class='comment-reply-link' href='".$main_name, $mainContent );
 $mainContent = str_replace('74013', '14234', $mainContent );
-//$mainContent = str_replace('wpjmr-submit-ratings', '', $mainContent );
-
-
-
-
 
 $mainContent = rand_podstav_smv($mainContent);
 
